f2eb6ef599f964c869f9f02772212613b8db5d43
developerlucasmarques <lucasmsantos819@gmail.com>
2024-02-16 18:05:34 -0300

    feat: make the id property mandatory in the Entity

--- src/shared/domain/entity.ts
+++ src/shared/domain/entity.ts
@@ -1,6 +1,6 @@
 import { type UniqueEntityID } from './unique-entity-id'
 
-export abstract class Entity<T extends { id?: UniqueEntityID }> {
+export abstract class Entity<T extends { id: UniqueEntityID }> {
   protected constructor (private readonly props: T) {}
 
   public getProp<K extends keyof T>(key: K): T[K] {
