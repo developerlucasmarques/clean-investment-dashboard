df86d8cf8ea57576f79b03f5b4d4071ab5882263
developerlucasmarques <lucasmsantos819@gmail.com>
2024-02-16 18:39:23 -0300

    style: fix for eslint rules

--- src/domain/entities/user/events/user-created.ts
+++ src/domain/entities/user/events/user-created.ts
@@ -4,6 +4,7 @@ import { type User } from '../user'
 export class UserCreated implements DomainEvent {
   public readonly dateTimeOccurred: Date
   private readonly _user: string
+
   public constructor (user: User) {
     this.dateTimeOccurred = new Date()
     this._user = JSON.stringify(user)
