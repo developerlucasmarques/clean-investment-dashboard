0ee997c086c6273a55771eb7e767604890778f11
developerlucasmarques <lucasmsantos819@gmail.com>
2024-02-26 18:32:36 -0300

    feat: persist User

--- src/modules/user/infra/repository/user-repository.ts
+++ src/modules/user/infra/repository/user-repository.ts
@@ -1,6 +1,11 @@
+import { type EntityManager } from '@mikro-orm/postgresql'
 import { type User } from 'modules/user/domain/entity/user'
 import { type IUserRepository } from 'modules/user/domain/repository/user-repository'
 
 export class UserRepository implements IUserRepository {
-  public async add (user: User): Promise<void> {}
+  public constructor (private readonly entityManager: EntityManager) {}
+
+  public async add (user: User): Promise<void> {
+    this.entityManager.persist(user)
+  }
 }
