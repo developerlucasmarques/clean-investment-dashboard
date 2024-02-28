967dd033b4c1de170222794cb8f55fcf56059b9f
developerlucasmarques <lucasmsantos819@gmail.com>
2024-02-28 10:35:34 -0300

    refactor: change type naming

--- src/shared/core/either.ts
+++ src/shared/core/either.ts
@@ -1,34 +1,29 @@
 import { type CoreError } from './core-error'
 
-export type Either<E extends CoreError, H> = Left<E, H> | Right<E, H>
+export type Either<L extends CoreError, R> = Left<L, R> | Right<L, R>
 
-interface Eithers<E extends CoreError, H> {
-  isLeft: () => this is Left<E, H>
-  isRight: () => this is Right<E, H>
+class Left<L extends CoreError, R> {
+  constructor (readonly value: L) {}
+
+  isLeft (): this is Left<L, R> { return true }
+
+  isRight (): this is Right<L, R> { return false }
 }
 
-class Left<E extends CoreError, H> implements Eithers<E, H> {
-  constructor (readonly value: E) {}
+class Right<L extends CoreError, R> {
+  constructor (readonly value: R) {}
 
-  isLeft (): this is Left<E, H> { return true }
+  isLeft (): this is Left<L, R> { return false }
 
-  isRight (): this is Right<E, H> { return false }
+  isRight (): this is Right<L, R> { return true }
 }
 
-class Right<E extends CoreError, H> implements Eithers<E, H> {
-  constructor (readonly value: H) {}
-
-  isLeft (): this is Left<E, H> { return false }
-
-  isRight (): this is Right<E, H> { return true }
+export const left = <const L extends CoreError, const R> (error: L): Left<L, R> => {
+  return new Left<L, R>(error)
 }
 
-export const left = <const E extends CoreError, const H> (error: E): Left<E, H> => {
-  return new Left<E, H>(error)
-}
-
-export function right<const E extends CoreError, const H extends void> (result?: H): Right<E, H>
-export function right<const E extends CoreError, const H> (result: H): Right<E, H>
-export function right<const E extends CoreError, const H> (result: H): Right<E, H> {
-  return new Right<E, H>(result)
+export function right<const L extends CoreError, const R extends void> (result?: R): Right<L, R>
+export function right<const L extends CoreError, const R> (result: R): Right<L, R>
+export function right<const L extends CoreError, const R> (result: R): Right<L, R> {
+  return new Right<L, R>(result)
 }
