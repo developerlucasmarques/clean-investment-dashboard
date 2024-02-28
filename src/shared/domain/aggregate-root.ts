8935c08069434cb0db8560f5a309d2c2db295543
developerlucasmarques <lucasmsantos819@gmail.com>
2024-02-28 18:10:48 -0300

    feat: add uncommittedEvents and _dispatchedEvents

--- src/shared/domain/aggregate-root.ts
+++ src/shared/domain/aggregate-root.ts
@@ -3,13 +3,24 @@ import { type DomainEvent } from './events'
 import { type UniqueEntityID } from './unique-entity-id'
 
 export abstract class AggregateRoot<T extends { id: UniqueEntityID }> extends Entity<T> {
-  private readonly _domainEvents: Array<DomainEvent<T>> = []
+  private readonly _domainEvents: Set<DomainEvent<T>> = new Set<DomainEvent<T>>()
+  private readonly _dispatchedEvents: Set<DomainEvent<T>> = new Set<DomainEvent<T>>()
 
   get domainEvents (): Array<DomainEvent<T>> {
-    return this._domainEvents
+    return Array.from(this._domainEvents)
+  }
+
+  get uncommittedEvents (): Array<DomainEvent<T>> {
+    return this.domainEvents.filter((event) => {
+      return !this._dispatchedEvents.has(event)
+    })
+  }
+
+  protected markEventsAsDispatched (domainEvent: DomainEvent<T>): void {
+    this._dispatchedEvents.add(domainEvent)
   }
 
   protected addDomainEvent (domainEvent: DomainEvent<T>): void {
-    this._domainEvents.push(domainEvent)
+    this._domainEvents.add(domainEvent)
   }
 }
