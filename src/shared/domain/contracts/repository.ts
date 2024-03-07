4e5536721d98fd2887f1a98835799147add8136d
developerlucasmarques <lucasmsantos819@gmail.com>
2024-03-07 10:35:36 -0300

    feat: create SearchableRepository and move repository to contracts folder

---
+++ src/shared/domain/contracts/repository.ts
@@ -0,0 +1,9 @@
+import { type AggregateRoot } from '../aggregate-root'
+
+export interface AddRepository<T extends AggregateRoot<any>> {
+  add: (aggregate: T) => Promise<void>
+}
+
+export interface SearchableRepository<in SearchInput, out SearchOutput> {
+  search: (input: SearchInput) => Promise<SearchOutput>
+}
--- src/shared/domain/repository.ts
+++
@@ -1,5 +0,0 @@
-import { type AggregateRoot } from './aggregate-root'
-
-export interface AddRepository<T extends AggregateRoot<any>> {
-  add: (aggregate: T) => Promise<void>
-}
